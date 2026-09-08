0d4904a6e37f275d8f8c556cfab839e931345715
cahuicHLT <lcahuich@hotel-shops.com>
2026-09-08 17:31:32 -0500

    fix: update AppDelegate and SceneDelegate for implicit Flutter engine initialization & bump version to 1.1.0+13; add Codemagic configuration

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,15 +2,15 @@ import Flutter
 import UIKit
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
-  lazy var flutterEngine = FlutterEngine(name: "my_flutter_engine")
-
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    flutterEngine.run()
-    GeneratedPluginRegistrant.register(with: self.flutterEngine)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -2,22 +2,5 @@ import Flutter
 import UIKit
 
 class SceneDelegate: FlutterSceneDelegate {
-  override func scene(
-    _ scene: UIScene,
-    willConnectTo session: UISceneSession,
-    options connectionOptions: UIScene.ConnectionOptions
-  ) {
-    guard let windowScene = (scene as? UIWindowScene) else { return }
 
-    let appDelegate = UIApplication.shared.delegate as! AppDelegate
-    let flutterEngine = appDelegate.flutterEngine
-    let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
-
-    let window = UIWindow(windowScene: windowScene)
-    window.rootViewController = flutterViewController
-    self.window = window
-    window.makeKeyAndVisible()
-
-    super.scene(scene, willConnectTo: session, options: connectionOptions)
-  }
 }
